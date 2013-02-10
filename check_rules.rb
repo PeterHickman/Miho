d3a80426f354730cbb1fb3d998ecfb94e303f778
Peter Hickman <peterhickman386@googlemail.com>
2013-02-10 18:11:28 +0000

    handle a list of files

--- check_rules.rb
+++ check_rules.rb
@@ -1,26 +1,29 @@
 #!/usr/bin/env ruby
 
-filename = ARGV[0]
+ARGV.each do |filename|
+  puts "Processing #{filename}"
 
-$rule_number = 0
-$patterns = Hash.new
+  $rule_number = 0
+  $patterns = Hash.new
 
-def learn(*patterns, &block)
-  $rule_number += 1
+  def learn(*patterns, &block)
+    $rule_number += 1
 
-  patterns.map{|x| x.downcase.strip.gsub(/_/, "*")}.each do |pattern|
-    if $patterns.has_key?(pattern)
-      puts "Rule #{$rule_number} duplicates #{$patterns[pattern]} with '#{pattern}'"
-    else
-      $patterns[pattern] = $rule_number
+    patterns.map{|x| x.downcase.strip.gsub(/_/, "*")}.each do |pattern|
+      if $patterns.has_key?(pattern)
+        puts "Rule #{$rule_number} duplicates #{$patterns[pattern]} with '#{pattern}'"
+      else
+        $patterns[pattern] = $rule_number
+      end
     end
   end
-end
 
-begin
-  eval(File.open(filename).read)
-rescue Exception => e
-  puts "Error found at #{$rule_number}: #{e}"
-end
+  begin
+    eval(File.open(filename).read)
+  rescue Exception => e
+    puts "Error found at #{$rule_number}: #{e}"
+  end
 
-puts "There are #{$patterns.size} unique patterns for #{$rule_number} rules"
+  puts "There are #{$patterns.size} unique patterns for #{$rule_number} rules"
+  puts
+end
