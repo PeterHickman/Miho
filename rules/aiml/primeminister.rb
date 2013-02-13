795b40c7bca71e99adb3277e27219f0f9a14e3bf
Peter Hickman <peterhickman386@googlemail.com>
2013-02-13 21:55:59 +0000

    another set of rules

---
+++ rules/aiml/primeminister.rb
@@ -0,0 +1,110 @@
+# Free software (c) 2011 ALICE A.I. Foundation.   
+# This program is open source code released under 
+# the terms of the GNU General Public License     
+# as published by the Free Software Foundation.   
+# Complies with AIML 1.0 Tag Set Specification 
+# as adopted by the ALICE A.I. Foundation.  
+# Last modified 10/5/2011 
+
+learn "DAVID CAMERON" do
+  "The Prime Minister?"
+end
+
+learn "WHAT IS THE PRIME MINISTER" do
+  "He is the political leader of a country. I wouldn\'t want to do his job."
+end
+
+learn "WHAT IS DOWNING STREET" do
+  "It\'s where the Prime Minister of England lives. He lives at 10 Downing Street."
+end
+
+learn "I HATE THE PRIME MINISTER" do
+  "You seem to feel strongly about him. Tell me about your political beliefs."
+end
+
+learn "I AM THE PRIME MINISTER" do
+  "Pleased to meet you Sir. How is life in Downing Street?"
+end
+
+learn "DO YOU LIKE DAVID CAMERON" do
+  "He is our democratically elected leader and I wish him well as Prime Minister."
+end
+
+# learn "NO" do
+# <that>THE PRIME MINISTER</that>
+#   "Who is the Prime Minister then? I thought he was called David Cameron."
+# end
+
+# learn "HE IS" do
+# <that>HE USED TO BE THE PRIME MINISTER</that>
+#   "<think>David Cameron</set></think>No he\'s not. The Prime Minister now is called David Cameron."
+# end
+
+learn "GORDON BROWN", "WHO IS GORDON BROWN" do
+  "He used to be the Prime Minister."
+end
+
+learn "WHO IS PRIME MINISTER OF BRITAIN" do
+  "David Cameron."
+end
+
+learn "WHO IS PRIME MINISTER OF ENGLAND" do
+  "David Cameron."
+end
+
+learn "WHO IS PRIME MINISTER OF UK" do
+  "David Cameron."
+end
+
+learn "WHO IS PRIME MINISTER OF *" do
+  "David Cameron. I think."
+end
+
+learn "WHO IS PRIME MINISTER" do
+  "David Cameron."
+end
+
+learn "WHO IS THE BRITISH PRIME MINISTER" do
+  "David Cameron."
+end
+
+learn "WHO IS THE ENGLISH PRIME MINISTER" do
+  "David Cameron."
+end
+
+learn "WHO IS THE PRIME MINISTER _ UK" do
+  "David Cameron."
+end
+
+learn "WHO IS THE PRIME MINISTER OF ENGLAND" do
+  "David Cameron."
+end
+
+learn "WHO IS THE PRIME MINISTER OF *" do
+  "David Cameron. I think."
+end
+
+learn "WHO IS THE PRIME MINISTER *" do
+  "David Cameron."
+end
+
+learn "WHO IS DAVID CAMERON" do
+  "He is the leader of the Conservative party here in the UK and the current Prime Minister."
+end
+
+learn "WHO IS TONY BLAIR" do
+  "He was the Prime Minister of the U.K."
+end
+
+learn "WHO LIVES * 10 DOWNING STREET" do
+  "The Prime Minister of England."
+end
+
+learn "THE PRIME MINISTER" do
+  "David Cameron?"
+end
+
+# learn "YES" do
+# <that>THE PRIME MINISTER</that>
+#   "I thought so. Mr Cameron lives at 10 Downing Street."
+# end
