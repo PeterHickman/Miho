ee875f5436b42457bb70d215d61efed9b17a6c87
Peter Hickman <peterhickman386@googlemail.com>
2013-02-14 21:43:16 +0000

    another aiml file

---
+++ rules/aiml/sports.rb
@@ -0,0 +1,95 @@
+# Free software (c) 2011 ALICE A.I. Foundation.   
+# This program is open source code released under 
+# the terms of the GNU General Public License     
+# as published by the Free Software Foundation.   
+# Complies with AIML 1.0 Tag Set Specification 
+# as adopted by the ALICE A.I. Foundation.  
+# Last modified 10/5/2011 
+
+learn "EACH YEAR IN PRO BASEBALL THE *" do
+  "The Gold Glove."
+end
+
+learn "IF YOU ARE RIDING FAKIE INSIDE *" do
+  "Snowboarding."
+end
+
+learn "IF YOU ARE RIDING FAKIE *" do
+  "Snowboarding."
+end
+
+learn "WHAT SOCCER *" do
+  "I was born without the sports gene."
+end
+
+learn "DO YOU KNOW BASKETBALL", "WHAT IS BASKETBALL" do
+  "A game with tall players."
+end
+
+learn "TELL ME ABOUT BASEBALL", "WHAT IS BASEBALL" do
+  "A game played with a hard, rawhide covered ball and wooden bat by two opposing teams of nine or ten players each. It is played on a field with four bases forming a diamond-shaped circuit."
+end
+
+learn "WHAT IS SOCCER" do
+  "A game played with a round ball by two teams of eleven players on a field with a goal at either end; the ball is moved chiefly by kicking or by using any part of the body except the hands and arms."
+end
+
+learn "I LOVE BASEBALL" do
+  "I am not into sports that much."
+end
+
+learn "I PLAY BASEBALL" do
+  "Which position do you like to play?"
+end
+
+learn "I PLAY SOCCER" do
+  "You have to run very fast to be any good at."
+end
+
+learn "I PLAY VOLLEYBALL" do
+  "Does Voleyball take up a lot of your time?"
+end
+
+learn "I PLAY BASKETBALL" do
+  "You must be very tall to be good at Basketball."
+end
+
+learn "HOW MANY BASEBALL *" do
+  "I don\'t really get into sports that much."
+end
+
+learn "THEY PLAY BASKETBALL" do
+  "They do?"
+end
+
+learn "DO YOU PLAY BASEBALL" do
+  "BASEBALL doesn\'t interest me very much."
+end
+
+learn "DO YOU PLAY SOCCER" do
+  "I don\'t know how to play soccer."
+end
+
+learn "DO YOU PLAY BASKETBALL" do
+  "No I don\'t have the coordination for hoops."
+end
+
+learn "DO YOU WANT TO PLAY BASKETBALL" do
+  "I am all net baby."
+end
+
+learn "LIKE BASKETBALL" do
+  "I am into the Net."
+end
+
+learn "ARE YOU A FOOTBALL *" do
+  "I am not really into football."
+end
+
+learn "WHO IS THE GREATEST BASEBALL PLAYER *" do
+  "George Herman Ruth. Quite the Babe."
+end
+
+learn "WHO IS THE BEST SOCCER PLAYER" do
+  "Maradona is great. Sinsemillia is even better."
+end
