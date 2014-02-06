28bbfa63051b1d4c624784fbd73bd737c0ac6e6a
Peter Hickman <peterhickman386@googlemail.com>
2014-02-06 15:57:17 +0000

    now checking conditions

--- check_rules.rb
+++ check_rules.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 def set(*args)
 end
 
@@ -11,15 +12,21 @@ ARGV.each do |filename|
   def learn(*patterns, &block)
     $rule_number += 1
 
-    if patterns.last.class == Hash
-      patterns.pop
-    end
+    conditions = patterns.last.class == Hash ? patterns.pop : {}
 
     patterns.map{|x| x.downcase.strip.gsub(/_/, "*")}.each do |pattern|
+      unless $patterns.has_key?(pattern)
+        $patterns[pattern] = Hash.new
+      end
+
       if $patterns.has_key?(pattern)
-        puts "Rule #{$rule_number} duplicates #{$patterns[pattern]} with '#{pattern}'"
+        $patterns[pattern].each do |k, v|
+          if conditions == v
+            puts "Rule #{$rule_number} duplicates #{$patterns[pattern][:number]} with '#{pattern}'"
+          end
+        end
       else
-        $patterns[pattern] = $rule_number
+        $patterns[pattern][$rule_number] = conditions
       end
     end
   end
